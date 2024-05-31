7d32b3b3fd4ab0dbc40d07fce23bc790f27660ba
Kaeadzhaiev Rustam <belonoir@gmail.com>
2024-05-31 15:48:45 +0300

    act() warnings removed from header tests

--- src/__tests__/Header.test.jsx
+++ src/__tests__/Header.test.jsx
@@ -8,23 +8,24 @@ import { testingTools } from "../helperFuncs/testingTools"
 jest.spyOn(window, "scrollTo");
 
 describe("Header behaviour", () => {
-
-
     it("must render header component", ()=>{
         testingTools.renderWithReduxRouter(<Header/>)
         const headerElement = screen.getByRole("banner");
         expect(headerElement).toBeInTheDocument();
     })
 
-    it("must show correct work with <Link/>", () => {
+
+   it("must show correct work with <Link/>", async () => {
         testingTools.renderWithReduxRouter(<Header/>)
         const link = screen.getByRole("link");
         expect(link).toBeInTheDocument();
-        userEvent.click(link);
+        await act(async ()=>{
+            userEvent.click(link);
+        })
         expect(window.location.pathname).toBe("/")
     })
 
-    it("must show correct form element behaviour", () => {
+  it("must show correct form element behaviour", () => {
         testingTools.renderWithReduxRouter(<Header/>)
         const form = screen.getByRole("form");
         expect(form).toBeInTheDocument();
@@ -34,33 +35,34 @@ describe("Header behaviour", () => {
         expect(handleSubmit).toHaveBeenCalledTimes(1)
     })
 
-    it("must show correct input field behaviour", () => {
+      it("must show correct input field behaviour", () => {
         testingTools.renderWithReduxRouter(<Header/>)
         const input = screen.getByRole("textbox");
         expect(input).toBeInTheDocument();
         const handleChange = jest.fn();
         input.onchange = handleChange;
-        act(()=>{
-            fireEvent.change(input);
-        })
+        fireEvent.change(input);
         expect(handleChange).toHaveBeenCalledTimes(1);
     })
 
-    it("must not show clear button", () => {
+   it("must not show clear button", () => {
         testingTools.renderWithReduxRouter(<Header/>)
         const clearButton = screen.queryByText("x");
         expect(clearButton).not.toBeInTheDocument();
     })
 
-    it("must show clear button and button should work correct", () => {
+     it("must show clear button and button should work correct", async () => {
+        expect.assertions(2)
         testingTools.renderWithReduxRouter(<Header/>)
         const input = screen.getByRole("textbox");
-        userEvent.type(input, "Something");
+        await act(async ()=>{
+            userEvent.type(input, "Something");
+        })
         const clearButton = screen.queryByText("x");
         expect(clearButton).toBeInTheDocument();
-        userEvent.click(clearButton);
+        await act(async ()=>{
+            userEvent.click(clearButton);
+        })
+        expect(input).not.toHaveTextContent();
     })
-
-
-
 })
